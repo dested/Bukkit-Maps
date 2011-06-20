da8bb95f406e6992f776de67c714e893c55fc18b
Salvatore <dested@gmail.com>
2011-06-19 19:22:00 -0700

    Added support for adding light to a chunk

--- src/main/java/org/bukkit/Chunk.java
+++ src/main/java/org/bukkit/Chunk.java
@@ -9,53 +9,89 @@ import org.bukkit.entity.Entity;
  */
 public interface Chunk {
 
-    /**
-     * Gets the X-coordinate of this chunk
-     *
-     * @return X-coordinate
-     */
-    int getX();
+	/**
+	 * Gets the X-coordinate of this chunk
+	 * 
+	 * @return X-coordinate
+	 */
+	int getX();
 
-    /**
-     * Gets the Z-coordinate of this chunk
-     *
-     * @return Z-coordinate
-     */
-    int getZ();
+	/**
+	 * Gets the Z-coordinate of this chunk
+	 * 
+	 * @return Z-coordinate
+	 */
+	int getZ();
 
-    /**
-     * Gets the world containing this chunk
-     *
-     * @return Parent World
-     */
-    World getWorld();
+	/**
+	 * Gets the world containing this chunk
+	 * 
+	 * @return Parent World
+	 */
+	World getWorld();
 
-    /**
-     * Gets a block from this chunk
-     *
-     * @param x 0-15
-     * @param y 0-127
-     * @param z 0-15
-     * @return the Block
-     */
-    Block getBlock(int x, int y, int z);
+	/**
+	 * Gets a block from this chunk
+	 * 
+	 * @param x
+	 *            0-15
+	 * @param y
+	 *            0-127
+	 * @param z
+	 *            0-15
+	 * @return the Block
+	 */
+	Block getBlock(int x, int y, int z);
 
-    /**
-     * Capture thread-safe read-only snapshot of chunk data
-     * @return ChunkSnapshot
-     */
-    ChunkSnapshot getChunkSnapshot();
+	/**
+	 * Gets the light from this chunk
+	 * 
+	 * @param x
+	 *            0-15
+	 * @param y
+	 *            0-127
+	 * @param z
+	 *            0-15
+	 * @return the light level
+	 */
+	int getLight(int x, int y, int z);
 
-    /**
-     * Capture thread-safe read-only snapshot of chunk data
-     * @param includeMaxblocky - if true, snapshot includes per-coordinate maximum Y values
-     * @param includeBiome - if true, snapshot includes per-coordinate biome type
-     * @param includeBiomeTempRain - if true, snapshot includes per-coordinate raw biome temperature and rainfall
-     * @return ChunkSnapshot
-     */
-    ChunkSnapshot getChunkSnapshot(boolean includeMaxblocky, boolean includeBiome, boolean includeBiomeTempRain);
+	/**
+	 * Sets the light from this chunk
+	 * 
+	 * @param x
+	 *            0-15
+	 * @param y
+	 *            0-127
+	 * @param z
+	 *            0-15
+	 * 
+	 */
+	void setLight(int x, int y, int z, int light);
 
-    Entity[] getEntities();
+	/**
+	 * Capture thread-safe read-only snapshot of chunk data
+	 * 
+	 * @return ChunkSnapshot
+	 */
+	ChunkSnapshot getChunkSnapshot();
 
-    BlockState[] getTileEntities();
+	/**
+	 * Capture thread-safe read-only snapshot of chunk data
+	 * 
+	 * @param includeMaxblocky
+	 *            - if true, snapshot includes per-coordinate maximum Y values
+	 * @param includeBiome
+	 *            - if true, snapshot includes per-coordinate biome type
+	 * @param includeBiomeTempRain
+	 *            - if true, snapshot includes per-coordinate raw biome
+	 *            temperature and rainfall
+	 * @return ChunkSnapshot
+	 */
+	ChunkSnapshot getChunkSnapshot(boolean includeMaxblocky,
+			boolean includeBiome, boolean includeBiomeTempRain);
+
+	Entity[] getEntities();
+
+	BlockState[] getTileEntities();
 }
